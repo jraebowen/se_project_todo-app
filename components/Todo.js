f3295563beae9a285841fb447be6309cd03482f1
Jaimie Bowen <jraebowen@gmail.com>
2025-05-27 16:40:32 -0700

    refactor: create a todo class and generate event listeners in todo.js

---
+++ components/Todo.js
@@ -0,0 +1,42 @@
+class Todo {
+  constructor(data, selector) {
+    console.log(data);
+    this._data = data;
+    this._todoTemplate = document.querySelector(selector);
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    const todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  getView() {
+    //TODO - make these a config objects
+    this._todoElement = this._todoTemplate.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    this._generateCheckboxEl();
+    this._setEventListeners();
+
+    return this._todoElement;
+  }
+}
+export default Todo;
